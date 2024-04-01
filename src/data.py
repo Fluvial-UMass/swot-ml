7351e56abcef1898e150b15cda4071a7f68167d5
Ted Langhorst <ted.langhorst@gmail.com>
2024-04-01 19:37:39 -0400

    Barely working EALSTM data loader

--- src/data.py
+++ src/data.py
@@ -2,121 +2,138 @@ import pandas as pd
 import jax.numpy as jnp
 import numpy as np
 from scipy.ndimage import distance_transform_edt
+import xarray as xr
+import os
+from pathlib import Path
+
 
 def get_dataloaders(train_df, test_df, **kwargs):
     train = DataLoader(train_df, train=True, shuffle=True, **kwargs)
     test = DataLoader(test_df, train=False, scale=train.scale, **kwargs)
     return train, test
 
+import pandas as pd
+import xarray as xr
+import numpy as np
+
+
 class DataLoader:
-    def __init__(self,
-                 df: pd.DataFrame,
+    def __init__(self, 
+                 data_dir: Path, 
+                 basins: list,
                  features: list,
                  target: str,
+                 time_slice: slice,
+                 split_time: np.datetime64,
                  batch_size: int,
                  sequence_length: int,
                  train: bool,
                  discharge_col: str = None,
                  scale: pd.Series = None,
-                 shuffle: bool = False,
                  fill_and_weight: bool = False,
-                 split_time: bool = False):
-        """
-        Initializes the DataLoader for batching and optionally shuffling data.
-        """
-        if train:
-            df_norm, scale = _normalize_data(df, None, [target])
-        elif not train and scale:
-            df_norm, _ = _normalize_data(df, scale, [target])
-        else:
-            raise ValueError("Scale must be provided when not training.")
-            
-        self.xd = df_norm[features].copy().values
-        self.y = df_norm[target].copy().values
-        self.scale = scale
-
+                 calc_dt: bool = False):
+        
+        self.data_dir = data_dir
+        self.basins = basins
+        self.features = features
+        self.target = target
+        self.time_slice = time_slice
+        self.split_time = split_time
         self.batch_size = batch_size
         self.sequence_length = sequence_length
         self.train = train
-        self.shuffle = shuffle
         self.fill_and_weight = fill_and_weight
-        self.split_time = split_time
+        self.calc_dt = calc_dt
+
+        self.train_indices = {}
+        self.test_indices = {}
+        self.xd = {}
+        self.xs = {}
+        self.dt = {}
+        self.y = {}
         
-        if self.fill_and_weight and self.split_time:
-            raise ValueError("Both fill_and_weight and split_time cannot be True at the same time.")
-
-        self.w = None
-        if self.fill_and_weight:
-            intervals = _distance_to_closest_obs(self.xd)
-            self.w = 1 / intervals**2 # Weighting function could be tuned
-            self.xd = _fill_nan_obs(self.xd)
-
-        self.dt = None
-        if self.split_time:
-            self.dt = _distance_to_closest_obs(self.xd)
-            self.xd = _fill_nan_obs(self.xd)
-
-        #TESTING
-        n_basins = 1
-        n_basin_features = 1
-        self.xs = np.ones((n_basins, n_basin_features))
-
-        # properties for loss funcs
-        if train:
-            self.unscaled_q = df[discharge_col].copy().values
-        
-        if train:
-            self.indices = np.where(~np.isnan(self.y[:-sequence_length+1]))[0]
-            self.indices = self.indices[self.indices >= sequence_length]
-        else:
-            self.indices = np.arange(sequence_length, self.xd.shape[0])
-
-        self.dataset_size = np.sum(self.indices != 0)  # Number of valid samples
-
-
-    def __iter__(self) -> tuple[np.ndarray, ...]:
-        """
-        Iterates over the data, yielding batches
-
-        Yields:
-            Tuple[np.ndarray, ...]: A tuple containing batch arrays for ids_batch, xd_batch, y_batch,
-                and optionally w_batch, and dt_batch.
-        """
-        if self.shuffle:
-            np.random.shuffle(self.indices)
-
-        for start in range(0, self.dataset_size, self.batch_size):
-            # Create a 2D index array for slicing
-            end = min(start + self.batch_size, self.dataset_size)
-            batch_indices = self.indices[start:end]
-            batch_sequences = np.array([np.arange(i - self.sequence_length + 1, i + 1) for i in batch_indices])
-
-            batch_dict = {
-                'ids': batch_indices,
-                'xd': self.xd[batch_sequences],
-                'y': self.y[batch_sequences],
-            }
-
-            if self.xs is not None:
-                batch_dict['xs'] = self.xs[,:]
-    
-            if self.w is not None:
-                batch_dict['w'] = self.w[batch_sequences]
-            if self.dt is not None:
-                batch_dict['dt'] = self.dt[batch_sequences]
-            if self.train:
-                batch_dict['unscaled_q'] = self.unscaled_q[batch_sequences]
-    
-            yield batch_dict
-
+        self._load_data()
 
     def __len__(self):
-        """
-        Returns the number of batches the DataLoader will generate.
-        """
-        if self.shuffle:
-            return np.inf
-        return (self.dataset_size + self.batch_size - 1) // self.batch_size
+        if self.train:
+            return (self.n_train_samples + self.batch_size - 1) // self.batch_size
+        return
+    
+    def _load_basin_data(self, basin):
+        file_path = f"{self.data_dir}/time_series/{basin}.nc"
+        ds = xr.open_dataset(file_path).sel(date=self.time_slice)
+        df = ds.to_dataframe()[self.features+[self.target]]
+
+        self.xd[basin] = df[self.features].values
+        self.y[basin] = df[[self.target]].values
+
+        split_idx = np.where(ds['date']==self.split_time)[0][0]
+        train_ids = np.where(~np.isnan(self.y[basin][:split_idx]))[0]
+        self.train_indices[basin] = train_ids[train_ids >= self.sequence_length]
+        self.n_train_samples += len(self.train_indices[basin])
+        
+        self.test_indices[basin] = np.arange(split_idx+self.sequence_length,len(df))
+        self.n_test_samples += len(self.test_indices[basin])
+
+        ds.close()
+        return 
+    
+    def _load_attributes(self):
+        file_path = f"{self.data_dir}/attributes/attributes.csv"
+        return pd.read_csv(file_path, index_col="index")
+    
+    def _load_data(self):
+        attributes_df = self._load_attributes()
+
+        self.n_train_samples = 0
+        self.n_test_samples = 0
+        for basin in self.basins:
+            self._load_basin_data(basin)
+            
+            if basin in attributes_df.index:
+                self.xs[basin] = attributes_df.loc[basin].values
+    
+    def __iter__(self):
+        if self.train:
+            np.random.shuffle(self.basins)
+            idx_list = self.train_indices
+        else:
+            idx_list = self.test_indices
+
+        xd, xs, y = [], [], []
+        for basin in self.basins:
+            for idx in idx_list[basin]:
+                sequence_ids =  np.array(np.arange(idx - self.sequence_length + 1, idx + 1))
+
+                xd.append(self.xd[basin][sequence_ids])
+                y.append(self.y[basin][sequence_ids])
+                xs.append(self.xs[basin])
+
+                if len(xd) == self.batch_size:
+                    yield _create_batch_dict(xd,xs,y)
+                    xd, xs, y = [], [], []
+                    
+        #Yield the final partial batch 
+        if len(xd)>0:
+            yield _create_batch_dict(xd,xs,y)
+
+
+def _create_batch_dict(xd, xs, y):
+    batch_dict = {'xd': np.stack(xd),
+                  'y': np.stack(y),
+                  'xs': np.stack(xs)}
+    return batch_dict
+
+def _combine_slices(slice_list):
+        start_list = [s.start for s in slice_list]
+        if None in start_list: start = None
+        else: start = min(start_list)
+            
+        stop_list = [s.stop for s in slice_list]
+        if None in stop_list: stop = None
+        else: stop = min(stop_list)
+
+        return slice(start, stop)
 
 def _distance_to_closest_obs(arr):
     nan_mask = np.isnan(arr)
@@ -180,4 +197,4 @@ def _normalize_data(df, scale=None, min_max_columns=None):
             scale['scale'][col] = df[col].std()
         normalized_df[col] = (df[col] - scale['offset'][col]) / scale['scale'][col]
 
-    return normalized_df, scale     
+    return normalized_df, scale   
