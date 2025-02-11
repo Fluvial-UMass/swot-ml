4041eb6d10ff7a2eae8f3c46cef4afcf8020b92b
Ted <tmlanghorst@umass.edu>
2025-02-11 15:49:09 +0000

    Parallelized inference

--- src/run.py
+++ src/run.py
@@ -14,6 +14,7 @@ import signal
 from pathlib import Path
 import pickle
 
+
 from config import *
 from data import HydroDataset, HydroDataLoader
 from train import Trainer, load_last_state
@@ -94,7 +95,22 @@ def hyperparam_grid_search(config_yml:Path, idx, k_folds=None):
         # Check if we actually did some training this run.
         out_file = trainer.log_dir / "test_data.pkl"
         if (start_epoch != trainer.epoch) or (not out_file.is_file()):
-            eval_model(cfg, trainer.model, dataset, trainer.log_dir, run_predict=False, make_plots=False)
+            eval_model(cfg, trainer.model, dataset, trainer.log_dir, run_predict=False, run_train=False, make_plots=False)
+
+def load_prediction_model(run_dir, chunk_idx):
+    cfg, model, _ = load_model(run_dir)
+    train_dataset = HydroDataset(cfg) 
+
+    cfg['data_subset'] = 'predict'
+    cfg['basin_file'] = f'metadata/site_lists/predictions/chunk_{chunk_idx:02}.txt'
+    cfg['shuffle'] = False # No need to shuffle for inference
+
+    predict_dataset = HydroDataset(cfg, train_ds=train_dataset, use_cache=False)
+
+    eval_dir = run_dir / 'inference'
+    eval_dir.mkdir(parents=True, exist_ok=True)
+
+    return cfg, model, predict_dataset, eval_dir
 
 
 def make_all_plots(cfg, results, bulk_metrics, basin_metrics, data_subset, log_dir):
@@ -117,49 +133,94 @@ def make_all_plots(cfg, results, bulk_metrics, basin_metrics, data_subset, log_d
         fig.savefig(fig_dir / f"{target}_metrics_hist.png",  dpi=300)
 
 
-def eval_model(cfg, model, dataset, log_dir, dt_max=16, run_test=True, run_predict=True, make_plots=True):
-    def eval_data_subset(data_subset, run_eval, make_plots):
-        results_file = log_dir / f"{data_subset}_data.pkl"
-        if run_eval:
-            dataset.cfg['exclude_target_from_index'] = None
-            dataset.update_indices(data_subset)
-            dataloader = HydroDataLoader(cfg, dataset)
+def eval_model(cfg, model, dataset, log_dir, 
+               run_test: bool | str = True, 
+               run_predict: bool | str = True, 
+               run_train: bool | str = True, 
+               make_plots=True):
+    """
+    Evaluates a model on specified data subsets, calculates metrics, and optionally generates plots.
 
-            results = predict(model, dataloader, return_dt=True, quiet=cfg.get('quiet',True), denormalize=True)
+    Parameters:
+    ----------
+    cfg : dict
+        Configuration dictionary with parameters for the model and evaluation process.
+    model : object
+        The trained model to evaluate.
+    dataset : object
+        The dataset object containing data and methods for creating data subsets.
+    log_dir : Path
+        Path to the directory where results and logs will be saved.
+    run_test, run_predict, run_train : bool | str, optional
+        If `True`, evaluates the model on the data subset. If a string, it is treated 
+        as the output file stem for test/predict/train subset results. Defaults to `True`.
+    make_plots : bool, optional
+        If `True`, generates and saves evaluation plots for the specified subsets. 
+        Defaults to `True`.
 
+    Notes:
+    -----
+    - The `predict` subset does not generate plots, regardless of the value of `make_plots`.
+      These would be identical to test, as 'predict' covers the same basins and time period
+      as 'test' but without validation data.
+
+    Saves:
+    ------
+    - Pickled results, bulk metrics, and basin metrics for each data subset.
+    - Plots in the `log_dir` directory if `make_plots` is `True`.
+    """
+    
+    def eval_data_subset(data_subset, out_stem=None):
+        if out_stem is False:
+            return
+        elif isinstance(out_stem, str):
+            if out_stem[-4:]!='.pkl':
+                out_stem += '.pkl'
+            else:
+                print("Pass string")
+        else:
+            out_stem = f"{data_subset}_data.pkl"
+            
+        results_file = log_dir / out_stem
+        print(f'Evaluating {data_subset} subset and saving to: {results_file}')
+
+        dataset.cfg['exclude_target_from_index'] = None
+        dataset.update_indices(data_subset)
+        dataloader = HydroDataLoader(cfg, dataset)
+
+        results = predict(model, dataloader, quiet=cfg.get('quiet',True), denormalize=True)
+
+        if data_subset != "predict":
             bulk_metrics = get_all_metrics(results)
             basin_metrics = get_basin_metrics(results)
-        
-            with open(results_file, 'wb') as f:
-                pickle.dump((results, bulk_metrics, basin_metrics), f)
-
-        elif results_file.is_file():   
-            with open(results_file, 'rb') as f:
-                results, bulk_metrics, basin_metrics = pickle.load(f)
-
+            if make_plots:
+                make_all_plots(cfg, results, bulk_metrics, basin_metrics, data_subset, log_dir) 
+            out = (results, bulk_metrics, basin_metrics)
         else:
-            raise ValueError("Plot only option '--plot' requires prediction data already saved. Try '--test' which will run the model and then plot.")
-        
-        if make_plots:
-            make_all_plots(cfg, results, bulk_metrics, basin_metrics, data_subset, log_dir) 
+            out = results
 
-    eval_data_subset('test', run_test, make_plots)
-    eval_data_subset('predict', run_predict, False)
-    eval_data_subset('train', run_predict, make_plots)    
-     
+        with open(results_file, 'wb') as f:
+            pickle.dump(out, f)
+
+    eval_data_subset('test', run_test)
+    eval_data_subset('predict', run_predict)
+    eval_data_subset('train', run_train)    
+
 
 def main(args):
-    run_eval = True # Default
+    # Default values
+    run_test = run_predict = run_train = True
 
     if args.train:
         config_yml = Path(args.train).resolve()
-        cfg, model, run_dir, dataset = start_training(config_yml)
+        cfg, model, eval_dir, dataset = start_training(config_yml)
     elif args.continue_training:
         run_dir = Path(args.continue_training).resolve()
         cfg, model, dataset = continue_training(run_dir)
+        eval_dir = run_dir
     elif args.finetune:
         finetune_yml = Path(args.finetune).resolve()
-        cfg, model, run_dir, dataset = finetune(finetune_yml)
+        cfg, model, eval_dir, dataset = finetune(finetune_yml)
     elif args.grid_search:
         config_yml = Path(args.grid_search).resolve()
         hyperparam_grid_search(config_yml, args.grid_index, args.k_folds)
@@ -168,12 +229,14 @@ def main(args):
         run_dir = Path(args.test).resolve()
         cfg, model, _ = load_model(run_dir)
         dataset = HydroDataset(cfg) 
-    elif args.plot:
-        run_dir = Path(args.plot).resolve()
-        cfg, model, _ = load_model(run_dir)
-        dataset = None
-        run_eval = False
-    eval_model(cfg, model, dataset, run_dir, 16, run_eval, run_eval, True)
+        eval_dir = run_dir
+    elif args.prediction_model:
+        run_test = run_train = False
+        run_predict = f'chunk_{args.basin_chunk_index:02}'
+        run_dir = Path(args.prediction_model).resolve()
+        cfg, model, dataset, eval_dir = load_prediction_model(run_dir, args.basin_chunk_index)
+
+    eval_model(cfg, model, dataset, eval_dir, run_test, run_predict, run_train)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run model based on the command line arguments.")
@@ -196,9 +259,9 @@ if __name__ == '__main__':
     group.add_argument('--test',  
                     type=str, 
                     help='Path to directory with model to test.')
-    group.add_argument('--plot',  
-                    type=str, 
-                    help='Path to directory with model results data to plot.')
+    group.add_argument('--prediction_model',
+                       type=str,
+                       help='Path to directory with model to use for predictions.')
     
     # Add a new argument for grid search index
     parser.add_argument('--grid_index', 
@@ -211,6 +274,12 @@ if __name__ == '__main__':
                         type=int, 
                         default=None,
                         help='Number of folds for k-fold cross-validation (optional, can be used with --train or --continue)')
+    
+    # Add a new argument for prediction chunk index
+    parser.add_argument('--basin_chunk_index', 
+                        type=int, 
+                        help='Index of the chunked basin list to predict on',
+                        required='--prediction_model' in sys.argv)
     
     args = parser.parse_args()
 
