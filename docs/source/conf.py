73be6c4f74dade14351c09f106e3a6fb771bb361
Ted <tmlanghorst@umass.edu>
2025-02-24 19:30:07 +0000

    autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ exclude_patterns = ['**.ipynb_checkpoints', '_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_themer'
+html_theme = 'sphinx_rtd_theme'
 
 html_static_path = ['_static']
 
